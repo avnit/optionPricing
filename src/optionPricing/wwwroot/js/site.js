eb88d92619122ed52cdea19885338f14582cacc9
Avnit Bambah <abambah@gmail.com>
2016-02-19 09:45:15 -0600

    adding additional pages and jqxGrid .

--- src/optionPricing/wwwroot/js/site.js
+++ src/optionPricing/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿// Write your Javascript code.
+﻿// site.js
+var ele = document.getElementById("UserName");
+ele.innerHTML = "Avnit Bambah";
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+    main.style = "background-color: #888;";
+};
+main.onmouseleave = function () {
+    main.style = "background-color: #782;";
+};
